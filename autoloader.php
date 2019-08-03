b0c88b0d7baef60e60219ba7fb3b46db9bfd3760
artemiev <mixartemev@gmail.com>
2019-08-03 15:43:42 +0300

    autoloader min

--- autoloader.php
+++ autoloader.php
@@ -7,12 +7,7 @@ const VEN = 'vendor' . DIRECTORY_SEPARATOR;
 
 spl_autoload_register(function (string $class) :void
 {
-    $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $class) . EXT; // ns -> path
     $source = explode('\\', $class)[0] == APP_NS ? SRC : VEN;
-    $classPath = $source . $classPath;
-    if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . $classPath)) { // file_exists finds file only by full path
-        require $classPath; // require finds file by relative path
-    } else {
-        print 'no file: ' . $classPath;
-    }
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class) . EXT; // ns -> path
+    require $source . $class;
 });
