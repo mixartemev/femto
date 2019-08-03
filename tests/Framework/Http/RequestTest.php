a03d0fd5849d817c522e7aca4175405aa91084f6
artemiev <mixartemev@gmail.com>
2019-08-03 15:46:32 +0300

    phpunit

--- tests/Framework/Http/RequestTest.php
+++ tests/Framework/Http/RequestTest.php
@@ -2,9 +2,24 @@
 
 namespace Tests\Framework\Http;
 
+use Framework\Http\Request;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
-    //public function
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $_GET = [];
+        $_POST = [];
+    }
+
+    public function testEmpty(): void
+    {
+        $request = new Request();
+
+        self::assertEquals([], $request->getQueryParams());
+        self::assertNull($request->getParsedBody());
+    }
 }
